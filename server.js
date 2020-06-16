1f3c2ad139c9b6c64762364767be2c0015f8004f
xavierprasetyo <xavierprasetyo89@gmail.com>
2020-06-16 21:27:10 +0700

    starting back end

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const config = require("config");
+
+const app = express();
+
+const db = config.get("mongoURI");
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started at port ${port}`));
